82c8635fab1bab3d3522ed2392bf9cf6fe3ab20a
the_bisounours <sedorne.jayson@gmail.com>
2024-06-27 12:23:16 +0200

    checkstock.

---
+++ Client/Commands/Administrateurs/checkstock.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+const { Accounts } = require("../../Models");
+const paginations = require("../../Functions/paginations");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("checkstock")
+        .setDescription("Permet de regarder le stock.")
+        .setDMPermission(true)
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    category: "Administrateurs",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        const accounts = await Accounts.find();
+        if(accounts.length === 0) {
+            return await interaction.reply({
+                content: "Aucun compte disponible.",
+                ephemeral: true
+            });
+        };
+
+        const embeds = [];
+        for (let index = 0; index < accounts.length; index++) {
+            const account = accounts[index];
+            
+            embeds.push(
+                new EmbedBuilder()
+                .setTitle("Informations des comptes disponible")
+                .setFooter({
+                    text: "Made by the_bisounours",
+                    iconURL: client.user.displayAvatarURL()
+                })
+                .setTimestamp()
+                .setColor("Blurple")
+                .setDescription(`> **Identifiant:** \`${account.accountId}\`\n> **Service:** \`${account.service}\`\n> **Username:** \`${account.username}\`\n> **Password:** \`${account.password}\`\n> **Compte générer:** <t:${Math.round(account.createdAt / 1000)}:D> <t:${Math.round(account.createdAt / 1000)}:R>`)
+            )
+        };
+
+        return await paginations(interaction, embeds, 60 * 1000, false);
+    }
+};
